b6ae7256055dde79056274e47a77b5b68872571c
dhfromkorea <dhllee424@gmail.com>
2014-07-08 10:55:47 -0700

    tried moz contact access; buggy. will merge with the old

--- platforms/firefoxos/www/js/services.js
+++ platforms/firefoxos/www/js/services.js
@@ -4,47 +4,29 @@ angular.module('starter.services', [])
  * A simple example service that returns some data.
  */
 .factory('Call', function($http) {
-  var makeCall = function(numbers) {
-    console.log('numbers', numbers);
-    return $http({
-      method: 'POST',
-      url: 'http://dialer.ngrok.com/call',
-      data: JSON.stringify(numbers),
-      dataType: "json"
-      })
-      .then(function(resp) {
-        return resp.data;
-      });
-  };
-  return {
-    makeCall: makeCall
-  };
-})
-  .factory('Friends', function() {
-    // Might use a resource here that returns a JSON array
+        var makeCall = function(numbers) {
+            console.log('numbers', numbers);
+            return $http({
+                    method: 'POST',
+                    url: 'http://simple-dialer.herokuapp.com/call',
+                    data: JSON.stringify(numbers),
+                    dataType: "json"
+                })
+                .then(function(resp) {
+                    return resp.data;
+                });
+        };
+        return {
+            makeCall: makeCall
+        };
+    })
+    .factory('Friends', function() {
+        // Might use a resource here that returns a JSON array
 
-    // Some fake testing data
-    var friends = [{
-      id: 0,
-      name: 'Scruff McGruff'
-    }, {
-      id: 1,
-      name: 'G.I. Joe'
-    }, {
-      id: 2,
-      name: 'Miss Frizzle'
-    }, {
-      id: 3,
-      name: 'Ash Ketchum'
-    }];
+    
+        var getAllContacts = window.navigator.mozContacts.getAll;
 
-    return {
-      all: function() {
-        return friends;
-      },
-      get: function(friendId) {
-        // Simple index lookup
-        return friends[friendId];
-      }
-    }
-  });
+        return {
+            getAllContacts: getAllContacts
+        }
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,38 +1,54 @@
 angular.module('starter.controllers', [])
-.controller('DashCtrl', function($scope, Call) {
-  $scope.numbers = {};
-  $scope.numbers.dst = null;
-  $scope.numbers.src = null || "14155345337";
-  $scope.addInput = function(val){
-    console.log('adding input');
-    $scope.numbers.dst = !$scope.numbers.dst ? val.toString() : $scope.numbers.dst + val.toString();
-  };
-  $scope.removeInput = function(){
-    console.log('removing input');
-    $scope.numbers.dst = $scope.numbers.dst.slice(0, $scope.numbers.dst.length-1);
-  };
-  $scope.makeCall = function() {
-  console.log('dialing a number');
-    Call.makeCall($scope.numbers)
-      .then(function(result) {
-        console.log(result, 'result');
-      })
-      .
-    catch (function(err) {
-      if (err) {
-        console.log('there is an error with the click-to-call featurea')
-        throw err;
-      }
-    });
-  };
-})
+    .controller('DashCtrl', function($scope, Call) {
+        $scope.numbers = {};
+        $scope.numbers.dst = null;
+        $scope.numbers.src = null || "14157588395";
+        $scope.addInput = function(val) {
+            console.log('adding input');
+            $scope.numbers.dst = !$scope.numbers.dst ? val.toString() : $scope.numbers.dst + val.toString();
+        };
+
+        $scope.removeInput = function() {
+            console.log('removing input');
+            $scope.numbers.dst = $scope.numbers.dst.slice(0, $scope.numbers.dst.length - 1);
+        };
+        $scope.makeCall = function() {
+            console.log('dialing a number');
+            Call.makeCall($scope.numbers)
+                .then(function(result) {
+                    console.log(result, 'result');
+                })
+                .
+            catch(function(err) {
+                if (err) {
+                    console.log('there is an error with the click-to-call featurea')
+                    throw err;
+                }
+            });
+        };
+    })
 
 .controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+
+    $scope.friends = function() {
+        var filter = {
+            // sortBy: givenName,
+            // sortOrder: ascending
+        }
+        var result = Friends.getAllContacts(filter);
+        result.onsuccess = function() {
+            console.log(this.result.length + ' contacts found.');
+        }
+        result.onerror = function() {
+            console.log('Something goes wrong!');
+        }
+    };
+    //initialize
+    $scope.friends();
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+// .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//     $scope.friend = Friends.get($stateParams.friendId);
+// })
 
 .controller('AccountCtrl', function($scope) {});
