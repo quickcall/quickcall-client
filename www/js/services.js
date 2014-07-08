1af8bba999df7f4655deb13b32dd670674a3393b
dhfromkorea <dhllee424@gmail.com>
2014-07-07 19:13:47 -0700

    - outbound ++ user can input the number ++ user can click a button to call with the input number ++ on click: app can retrieve the number and send a POST req to server

--- www/js/services.js
+++ www/js/services.js
@@ -3,24 +3,48 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
-
+.factory('Call', function($http) {
+  var makeCall = function(numbers) {
+    console.log('numbers', numbers);
+    return $http({
+      method: 'POST',
+      url: 'http://dialer.ngrok.com/call',
+      data: JSON.stringify(numbers),
+      dataType: "json"
+      })
+      .then(function(resp) {
+        return resp.data;
+      });
+  };
   return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
+    makeCall: makeCall
+  };
+})
+  .factory('Friends', function() {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var friends = [{
+      id: 0,
+      name: 'Scruff McGruff'
+    }, {
+      id: 1,
+      name: 'G.I. Joe'
+    }, {
+      id: 2,
+      name: 'Miss Frizzle'
+    }, {
+      id: 3,
+      name: 'Ash Ketchum'
+    }];
+
+    return {
+      all: function() {
+        return friends;
+      },
+      get: function(friendId) {
+        // Simple index lookup
+        return friends[friendId];
+      }
     }
-  }
-});
+  });
