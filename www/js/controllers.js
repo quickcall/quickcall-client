62bf43ef680767446e961d9f1013846c05625bb8
dhfromkorea <dhllee424@gmail.com>
2014-07-08 00:03:58 -0700

    - UI ++ have 3 tabs (call, contact, config) ++ call tab: dial input, display dst number, call button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,18 @@
 angular.module('starter.controllers', [])
-
 .controller('DashCtrl', function($scope, Call) {
   $scope.numbers = {};
+  $scope.numbers.dst = null;
+  $scope.numbers.src = null || "14155345337";
+  $scope.addInput = function(val){
+    console.log('adding input');
+    $scope.numbers.dst = !$scope.numbers.dst ? val.toString() : $scope.numbers.dst + val.toString();
+  };
+  $scope.removeInput = function(){
+    console.log('removing input');
+    $scope.numbers.dst = $scope.numbers.dst.slice(0, $scope.numbers.dst.length-1);
+  };
   $scope.makeCall = function() {
+  console.log('dialing a number');
     Call.makeCall($scope.numbers)
       .then(function(result) {
         console.log(result, 'result');
