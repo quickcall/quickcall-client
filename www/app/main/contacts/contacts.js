0d7005cf8129d282c1d56b72c2b2c73ddd8804a8
Kia Fathi <kiafathi@me.com>
2014-07-10 16:41:59 -0700

    Implement base contacts view

--- www/app/main/contacts/contacts.js
+++ www/app/main/contacts/contacts.js
@@ -12,4 +12,30 @@ angular.module('app.main.contacts', [
       }
     });
 })
-.controller('ContactsCtrl', function($scope){});
+.controller('ContactsCtrl', function($scope, $ionicModal){
+  console.log('Contacts CTRl linked!');
+  $scope.friends = [
+    {
+      name: "Alexander Phillip",
+      imgPath: 'alexPhillip.jpeg',
+      description: "AWESOME!",
+      phoneNumber: "415-514-1234"
+    },
+    {
+      name: "Barack The Rock Obama",
+      imgPath: 'obama.jpeg',
+      description: "Yo America, the beautiful",
+      phoneNumber: "USA-USA-USASA"
+    },
+    {
+      name: "DH From Korea",
+      imgPath: "dhLee.jpeg",
+      description: "QuickCall founder, DH From Korea, DH From Korea, DH From Korea",
+      phoneNumber: "123-123-1234"
+    }
+  ];
+  $scope.showModal = function(){
+    console.log(this.modal);
+    console.log(this);
+  };
+});
