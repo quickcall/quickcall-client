f1f71838cc1f98de7f96e38dd9184029702c1e76
Kia Fathi <kiafathi@me.com>
2014-07-11 11:58:52 -0700

    Add basic popup, for when call is invoked

--- www/app/services/services.js
+++ www/app/services/services.js
@@ -1,8 +1,19 @@
 angular.module('app.services', [
 ])
-.factory('DialerFactory', function ($http) {
+.factory('DialerFactory', function ($http, $ionicPopup) {
   var call = function(number) {
     console.log(number);
+
+
+     var alertPopup = $ionicPopup.alert({
+       title: 'Don\'t eat that!',
+       template: 'It might taste good'
+     });
+     alertPopup.then(function(res) {
+       console.log('Thank you for not eating my delicious ice cream cone');
+     });
+  
+
     return $http({
       method: 'POST',
       url: 'http://simple-dialer.herokuapp.com/call',
