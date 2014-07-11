e6c9dd14d87f1de3f093ba84fff9a10629ccfdd9
Kia Fathi <kiafathi@me.com>
2014-07-11 12:03:35 -0700

    Add pop-up upon calling in dialer services

--- www/app/services/services.js
+++ www/app/services/services.js
@@ -2,17 +2,11 @@ angular.module('app.services', [
 ])
 .factory('DialerFactory', function ($http, $ionicPopup) {
   var call = function(number) {
-    console.log(number);
 
-
-     var alertPopup = $ionicPopup.alert({
-       title: 'Don\'t eat that!',
-       template: 'It might taste good'
-     });
-     alertPopup.then(function(res) {
-       console.log('Thank you for not eating my delicious ice cream cone');
-     });
-  
+    var alertPopup = $ionicPopup.alert({
+      title: 'Calling...',
+      template: 'Number: ' + number
+    });
 
     return $http({
       method: 'POST',
