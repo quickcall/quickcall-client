d6a6b4aff9244c40431868b7d6da6d89d5b95c52
Mason <maseh87@gmail.com>
2014-07-11 17:12:20 -0700

    added new factory

--- www/app/main/dialer/dialer.js
+++ www/app/main/dialer/dialer.js
@@ -18,7 +18,7 @@ angular.module('app.main.dialer', [
 
   $scope.addInput = function(num) {
     $scope.phoneNumber += num;
-    console.log(ContactsFactory.friends);
+    console.log(mySwiper);
   };
   $scope.makeCall = function() {
     DialerFactory.call($scope.phoneNumber);
@@ -31,4 +31,50 @@ angular.module('app.main.dialer', [
     restrict: 'E',
     templateUrl: 'app/main/dialer/dialer-directive.html'
   };
+})
+
+.directive('dialerShrink', function($document) {
+  var fadeAmt;
+
+  var shrink = function(header, content, amt, max) {
+    amt = Math.min(44, amt);
+    fadeAmt = 1 - amt / 44;
+    ionic.requestAnimationFrame(function() {
+      header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, -' + amt + 'px, 0)';
+      for(var i = 0, j = header.children.length; i < j; i++) {
+        header.children[i].style.opacity = fadeAmt;
+      }
+    });
+  };
+
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+      var starty = $scope.$eval($attr.headerShrink) || 0;
+      var shrinkAmt;
+
+      var dialer = $document[0].body.querySelector('dialer');
+      var dialerHeight = dialer.offsetHeight;
+
+      $element.bind('scroll', function(e) {
+        if(e.detail.scrollTop > starty) {
+          // Start shrinking
+          shrinkAmt = headerHeight - Math.max(0, (starty + headerHeight) - e.detail.scrollTop);
+          shrink(header, $element[0], shrinkAmt, headerHeight);
+        } else {
+          shrink(header, $element[0], 0, headerHeight);
+        }
+      });
+    }
+  };
 });
+
+
+
+
+
+
+
+
+
+
