741a7c01cceaaacf3cd1acb5ef3b89d06c25f587
Mason <maseh87@gmail.com>
2014-07-13 15:16:58 -0700

    added polymer paper element to dialer.html

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/mason/Desktop/hr/QuickCall/quickcall-client/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/mason/Desktop/hr/QuickCall/quickcall-client/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/mason/Desktop/hr/QuickCall/quickcall-client/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
+/Users/mason/Desktop/hr/QuickCall/quickcall-client/platforms/android/ant-build/HelloCordova.ap_ \
+/Users/mason/Desktop/hr/QuickCall/quickcall-client/platforms/android/ant-build/classes.dex \
+/Users/mason/Desktop/hr/QuickCall/quickcall-client/platforms/android/CordovaLib/ant-build/classes.jar \
