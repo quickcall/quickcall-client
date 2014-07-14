356c86e45aeed62b6fc9aa61ba57466a18fbd002
Kia Fathi <kiafathi@me.com>
2014-07-14 11:45:24 -0700

    Fix dialer phone number

--- www/app/main/dialer/dialer.js
+++ www/app/main/dialer/dialer.js
@@ -47,62 +47,6 @@ angular.module('app.main.dialer', [
     $scope.phoneNumber = $scope.phoneNumber.slice(0,-1);
   };
 
-  $scope.friends = [
-    {
-      name: "Alexander Phillip",
-      imgPath: 'alexPhillip.jpeg',
-      description: "AWESOME!",
-      phoneNumber: "415-514-1234"
-    },
-    {
-      name: "Barack The Rock Obama",
-      imgPath: 'obama.jpeg',
-      description: "Yo America, the beautiful",
-      phoneNumber: "USA-USA-USASA"
-    },
-    {
-      name: "DH Lee",
-      imgPath: "dhLee.jpeg",
-      description: "HR14 Fullstack Software Engineer, QuickCall Founder",
-      phoneNumber: "14155345337"
-    },
-    {
-      name: "Kia Fathi",
-      imgPath: "kiaFathi.jpg",
-      description: "HR14 Fullstack Software Engineer",
-      phoneNumber: "16508888614"
-    },
-    {
-      name: "Jakob Harclerode",
-      imgPath: "yahkob.jpg",
-      description: "HR14 Fullstack Software Engineer, Heavy Metal",
-      phoneNumber: "19286996726"
-    },
-    {
-      name: "Mason Hargrove",
-      imgPath: "mase87.jpg",
-      description: "HR14 Fullstack Software Engineer, Straight-Up Badass",
-      phoneNumber: "12294128411"
-    },
-    {
-      name: "SpaceCat",
-      imgPath: "spaceCat.jpg",
-      description: "RARGH MEOW RARG! PEW PEW LAZORS",
-      phoneNumber: "YOU CAN NEVER GET THIS"
-    },
-    {
-      name: "Alexander Phillip",
-      imgPath: 'alexPhillip.jpeg',
-      description: "AWESOME!",
-      phoneNumber: "415-514-1234"
-    },
-    {
-      name: "Alexander Phillip",
-      imgPath: 'alexPhillip.jpeg',
-      description: "AWESOME!",
-      phoneNumber: "415-514-1234"
-    }
-  ];
   //modal stuff
   $ionicModal.fromTemplateUrl('app/main/dialer/dialer-directive.html', {
     scope: $scope,
