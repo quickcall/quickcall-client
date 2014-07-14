a5c39d5f66b8f93bca8a126040babb0cd49944b9
Mason <maseh87@gmail.com>
2014-07-14 16:06:31 -0700

    added recent contacts card on dialer view styled correctly

--- platforms/android/assets/www/app/services/services.js
+++ platforms/android/assets/www/app/services/services.js
@@ -1,9 +1,18 @@
 angular.module('app.services', [
 ])
 .factory('DialerFactory', function ($http, $ionicPopup) {
+  var recentNumbers = [];
+
   var call = function(destinationNumber) {
+    
+    recentNumbers.push(destinationNumber);
+    
+    if(recentNumbers.length > 3){
+      recentNumbers.shift();
+    }
 
     var sourceNumber = '';
+
 
     var serverData = {
       dst: destinationNumber,
@@ -33,6 +42,55 @@ angular.module('app.services', [
   };
 
   return {
-    call: call
+    call: call,
+    recentNumbers : recentNumbers
+  };
+}).factory('ContactsFactory', function(){
+  var contacts = [
+    {
+      name: "Alexander Phillip",
+      imgPath: 'alexPhillip.jpeg',
+      description: "AWESOME!",
+      phoneNumber: "415-514-1234"
+    },
+    {
+      name: "Barack The Rock Obama",
+      imgPath: 'obama.jpeg',
+      description: "Yo America, the beautiful",
+      phoneNumber: "USA-USA-USASA"
+    },
+    {
+      name: "DH Lee",
+      imgPath: "dhLee.jpeg",
+      description: "HR14 Fullstack Software Engineer, QuickCall Founder",
+      phoneNumber: "14155345337"
+    },
+    {
+      name: "Kia Fathi",
+      imgPath: "kiaFathi.jpg",
+      description: "HR14 Fullstack Software Engineer",
+      phoneNumber: "16508888614"
+    },
+    {
+      name: "Jakob Harclerode",
+      imgPath: "yahkob.jpg",
+      description: "HR14 Fullstack Software Engineer, Heavy Metal",
+      phoneNumber: "19286996726"
+    },
+    {
+      name: "Mason Hargrove",
+      imgPath: "mase87.jpg",
+      description: "HR14 Fullstack Software Engineer, Straight-Up Badass",
+      phoneNumber: "12294128411"
+    },
+    {
+      name: "SpaceCat",
+      imgPath: "spaceCat.jpg",
+      description: "RARGH MEOW RARG! PEW PEW LAZORS",
+      phoneNumber: "YOU CAN NEVER GET THIS"
+    }
+  ];
+  return {
+    contacts: contacts
   };
 });
