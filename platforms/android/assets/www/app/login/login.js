8d1a6cdd66050e54cc1786225d21e9916e0c0831
Mason <maseh87@gmail.com>
2014-07-17 14:59:47 -0700

    modified contacts info modal

--- platforms/android/assets/www/app/login/login.js
+++ platforms/android/assets/www/app/login/login.js
@@ -9,7 +9,35 @@ angular.module('app.login', [
       controller: 'LoginCtrl'
     });
 })
-.controller('LoginCtrl', function($scope, DialerFactory, $state){
+
+.controller('LoginCtrl', function($scope, DialerFactory, $state, $window, $http){
+  var user = $scope.currentUser;
+  $scope.submit = function(){
+    $http({
+      method: 'GET', 
+      url: 'http://quickcall-server.herokuapp.com/account',
+      params:{
+        authId:user.ID,
+        authToken:user.token
+      }
+    })
+    .success(function(data, status, headers, config) {
+      var tokens = parseInt(data.cash_credits)
+      if(data.auth_id === user.ID && tokens > 1){
+        $window.localStorage.setItem('com.quickCall.auth',
+        JSON.stringify({
+          id:user.ID,
+          token:user.token,
+          number:user.number
+        })
+        );
+      }
+    })
+    .error(function(data, status, headers, config) {
+      console.error(data);
+    });
+   
+  };  
   //Login establishes the currentUser property of DialerFactory, that is injected into other views
   $scope.currentUser = DialerFactory.currentUser;
 });
