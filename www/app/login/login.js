29fa0e8d7afa406c91a46d2a2482b9ee4cc4dbfb
Yahkob <jakobharclerode@gmail.com>
2014-07-17 13:56:41 -0700

    added plivo authenticate credentials

--- www/app/login/login.js
+++ www/app/login/login.js
@@ -13,13 +13,30 @@ angular.module('app.login', [
 .controller('LoginCtrl', function($scope, DialerFactory, $state, $window, $http){
   var user = $scope.currentUser;
   $scope.submit = function(){
-    $window.localStorage.setItem('com.quickCall.auth',
-    JSON.stringify({
-      id:user.ID,
-      token:user.token,
-      number:user.number
-    }
-    ));
+    $http({
+      method: 'GET', 
+      url: 'http://quickcall-server.herokuapp.com/account',
+      params:{
+        authId:user.ID,
+        authToken:user.token
+      }
+    })
+    .success(function(data, status, headers, config) {
+      var tokens = parseInt(data.cash_credits)
+      if(data.auth_id === user.ID && tokens > 1){
+        $window.localStorage.setItem('com.quickCall.auth',
+        JSON.stringify({
+          id:user.ID,
+          token:user.token,
+          number:user.number
+        })
+        );
+      }
+    })
+    .error(function(data, status, headers, config) {
+      console.error(data);
+    });
+   
   };  
   //Login establishes the currentUser property of DialerFactory, that is injected into other views
   $scope.currentUser = DialerFactory.currentUser;
